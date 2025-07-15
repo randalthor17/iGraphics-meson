71c490d336c300fa6673bf1adac9c1986f46b081
randalthor17 <itsmehauhon@gmail.com>
2025-07-15 17:50:45 +0600

    add updater

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+(cd ./include/iGraphics/ &&
+  curl -LO "https://raw.githubusercontent.com/mahirlabibdihan/Modern-iGraphics/refs/heads/main/iGraphics.h" &&
+  curl -LO "https://raw.githubusercontent.com/mahirlabibdihan/Modern-iGraphics/refs/heads/main/iSound.h" &&
+  curl -LO "https://raw.githubusercontent.com/mahirlabibdihan/Modern-iGraphics/refs/heads/main/iFont.h")
